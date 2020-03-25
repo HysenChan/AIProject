7a5f9553873e107d690897112d544ff90d4c29e5
HysenChan <17602059496@163.com>
2020-03-25 10:53:20 +0800

    【添加】多AI角色障碍赛

---
+++ Assets/Script/Manipulation/GenerateBots.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBots : MonoBehaviour
+{
+    public GameObject botPrefab;
+    public int botCount;
+    public GameObject target;
+    //长方形“盒子”定义了随机产生的AI的初始位置
+    public float minX = -10.0f;
+    public float maxX = 20.0f;
+    public float minZ = -10.0f;
+    public float maxZ = 20.0f;
+    public float Yvalue = 4.0f;
+
+    private void Start()
+    {
+        Vector3 spawnPosition;
+        GameObject bot;
+        for (int i = 0; i < botCount; i++)
+        {
+            //随机选择一个生成点，实例化预制体
+            spawnPosition = new Vector3(Random.Range(minX, maxX), Yvalue, Random.Range(minZ, maxZ));
+            bot = Instantiate(botPrefab, spawnPosition, Quaternion.identity) as GameObject;
+            bot.GetComponent<SteeringForArrive>().target = target;
+        }
+    }
+}
---
+++ Assets/Script/Manipulation/PlaceObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObjects : MonoBehaviour
+{
+    public GameObject objectsToPlace;
+    public int count;
+
+    //海鸥的初始位置在一个半径为radius的球体内随机产生
+    public float radius;
+    public bool isPlanar;
+
+    private void Awake()
+    {
+        Vector3 position = new Vector3(0, 0, 0);
+        for (int i = 0; i < count; i++)
+        {
+            position = transform.position + Random.insideUnitSphere * radius;
+            if (isPlanar)
+            {
+                position.y = objectsToPlace.transform.position.y;
+            }
+            //实例化海鸥的预制体
+            Instantiate(objectsToPlace, position, Quaternion.identity);
+        }
+    }
+}
